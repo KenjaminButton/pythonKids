14ed0bdc63d10137abdfa71bfadf879a17f75062
KendrickPC <kennethpchang@gmail.com>
2018-06-05 08:17:29 +0800

    chapter1 finished for both books and folders organized

---
+++ book2/chapter1/madlib.py
@@ -0,0 +1,5 @@
+adjective = input("Please enter an adjective: ")
+noun = input("Please enter a noun: ")
+verb = input("Please enter a verb ending in -ed: ")
+print("\nYour MadLib:")
+print("The", adjective, noun, verb, "over the lazy brown dog.")
---
+++ book2/chapter1/madlib2.py
@@ -0,0 +1,7 @@
+# Explain why you put a space at the end of the input
+adjective = input("Please enter an adjective: ")
+noun = input("Please enter a noun: ")
+verb = input("Please enter a verb ending in -ed: ")
+
+print("Your MadLib:")
+print("The", adjective, noun, verb, "over the fat black cat.")
