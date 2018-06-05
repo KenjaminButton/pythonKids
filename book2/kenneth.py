3d55a56e24d0ce73457a234ea43bfe02c570ba8b
KendrickPC <kennethpchang@gmail.com>
2018-06-05 08:08:34 +0800

    chapter 1 completed

---
+++ book2/kenneth.py
@@ -0,0 +1,10 @@
+# YourName.py
+
+
+name = input("What is your name?\n")
+print("Hi, ", name)
+
+# Have a fully functional Python programming environment and text editor
+# Enter programming commands directly into the Python shell
+# Write, save, run and modify short programs in IDLE
+
